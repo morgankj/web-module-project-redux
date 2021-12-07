36f45ef1a813aa5e4f3d18b29bebcef188f5695f
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-07 17:12:32 -0600

    You can now add movies to your favorites list and see them render there

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,18 +4,22 @@ import { useParams, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { deleteMovie } from '../actions/movieActions';
+import { addFavorite } from '../actions/favoritesActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies, displayFavorites, deleteMovie } = props;
-
-    const movie = movies.find(movie=>movie.id===Number(id));
+    const { movies, displayFavorites, addFavorite, deleteMovie } = props;
+    const movie = movies.find(movie => movie.id === Number(id));
 
     const handleDelete = () => {
         deleteMovie(movie.id);
         push('/movies');
+    }
+
+    const handleFavorite = () => {
+        addFavorite(movie);
     }
     
     return(<div className="modal-page col">
@@ -47,7 +51,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            {displayFavorites && <span className="m-2 btn btn-dark">Favorite</span>}
+                            {displayFavorites && <span onClick={handleFavorite} className="m-2 btn btn-dark">Favorite</span>}
                             <span onClick={handleDelete} className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
@@ -59,9 +63,9 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) => {
     return ({
-        displayFavorites: state.movieReducer.displayFavorites,
+        displayFavorites: state.favoritesReducer.displayFavorites,
         movies: state.movieReducer.movies
     })
 }
 
-export default connect(mapStateToProps, { deleteMovie })(Movie);
+export default connect(mapStateToProps, { deleteMovie, addFavorite })(Movie);
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -17,7 +17,7 @@ const MovieHeader = (props) => {
             <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-            <div onClick={handleClick} className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
+            <div onClick={handleClick} className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show" } Favorites</span></div>
             <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
             <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
         </div>
